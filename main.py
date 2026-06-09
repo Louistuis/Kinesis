42c65647786831c028146b01cec53d07a18de489
Louis <louis@mac.com>
2026-06-09 23:36:36 +0200

    Fix missing Theme import

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from rich.console import Console
 from rich.panel import Panel
 from rich.markdown import Markdown
 from rich.text import Text
+from rich.theme import Theme
 from rich.live import Live
 from rich.table import Table
 from rich.rule import Rule
